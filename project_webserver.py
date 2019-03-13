da03ea8a3518a0df6f4a808c8d1b4ef940981e0e
lemonylight7 <elizaveta.yermakova@gmail.com>
2019-03-14 00:26:00 +0300

    Final version: - user statistics - PEP8 fixes - minor fixes

--- project_webserver.py
+++ project_webserver.py
@@ -16,6 +16,7 @@ db = DB("project.db")
 ---===###  Useful functions  ###===---
 '''
 
+
 def make_thumbnail(infile, outfile):
     im = Image.open(infile)
     xsize, ysize = im.size
@@ -26,6 +27,7 @@ def make_thumbnail(infile, outfile):
     im = im.crop(box=crop_size)
     im.thumbnail((300, 300))
     im.save(outfile)
+
 
 def save_file(f):
     save_filename = "static/img/usr_{0}/{1}_{2}".format(
@@ -40,9 +42,11 @@ def save_file(f):
     make_thumbnail(save_filename, thmb_filename)
     return (save_filename, thmb_filename)
 
+
 def abort_if_post_not_found(post_id):
     if not PostsModel(db.get_connection()).get(post_id):
         abort(404, message="Post {} not found".format(post_id))
+
 
 def abort_if_not_authorized():
     if "userid" not in session:
@@ -53,10 +57,12 @@ def abort_if_not_authorized():
 ---===###  API classes and bindings  ###===---
 '''
 
+
 class Posts(Resource):
     def get(self, post_id):
         abort_if_post_not_found(post_id)
         abort_if_not_authorized()
+        # получаем одну запрошенную публикацию
         post = PostsModel(db.get_connection()).get(post_id)
         return jsonify({'post': post})
 
@@ -66,45 +72,56 @@ class Posts(Resource):
         parser = reqparse.RequestParser()
         parser.add_argument('title')
         args = parser.parse_args()
+        # изменияем описание на присланное в запросе post
         PostsModel(db.get_connection()).update_title(post_id, args['title'])
         return jsonify({'success': 'OK'})
 
     def delete(self, post_id):
         abort_if_post_not_found(post_id)
         abort_if_not_authorized()
+        # удаляем публикацию
         PostsModel(db.get_connection()).delete(post_id)
         return jsonify({'success': 'OK'})
 
 api.add_resource(Posts, '/api/posts/<int:post_id>')
 
+
 class Likes(Resource):
     def get(self, post_id):
         abort_if_post_not_found(post_id)
         abort_if_not_authorized()
+        # получаем количество лайков для данной публикации
         count = LikesModel(db.get_connection()).get_count(post_id)
+        # проверяем есть ли твой лайк под данной публикацией
         your = LikesModel(db.get_connection()).get_your(post_id, session['userid'])
         return jsonify({'count': count, 'your': your})
 
     def post(self, post_id):
         abort_if_post_not_found(post_id)
         abort_if_not_authorized()
+        # добавляем лайк
         LikesModel(db.get_connection()).insert(post_id, session['userid'])
+        # получаем актуальное количество лайков
         count = LikesModel(db.get_connection()).get_count(post_id)
         return jsonify({'count': count})
 
     def delete(self, post_id):
         abort_if_post_not_found(post_id)
         abort_if_not_authorized()
+        # удаляем лайк
         LikesModel(db.get_connection()).delete(post_id, session['userid'])
+        # получаем актуальное количество лайков
         count = LikesModel(db.get_connection()).get_count(post_id)
         return jsonify({'count': count})
 
 api.add_resource(Likes, '/api/likes/<int:post_id>')
 
+
 class Comments(Resource):
     def get(self, post_id):
         abort_if_post_not_found(post_id)
         abort_if_not_authorized()
+        # получаем данные для отображения описания и комментариев
         comments = CommentsModel(db.get_connection()).get(post_id)
         return jsonify({'comments': comments})
 
@@ -114,6 +131,7 @@ class Comments(Resource):
         parser = reqparse.RequestParser()
         parser.add_argument('comment')
         args = parser.parse_args()
+        # добавляем комментарий
         CommentsModel(db.get_connection()).insert(post_id, session['userid'], args['comment'])
         return jsonify({'success': 'OK'})
 
@@ -123,19 +141,31 @@ class Comments(Resource):
         parser = reqparse.RequestParser()
         parser.add_argument('comment_id')
         args = parser.parse_args()
+        # удаляем комментарий
         CommentsModel(db.get_connection()).delete(args['comment_id'], session['userid'])
         return jsonify({'success': 'OK'})
 
 api.add_resource(Comments, '/api/comments/<int:post_id>')
 
+
 class Users(Resource):
     def get(self, user_id):
         abort_if_not_authorized()
-        parser = reqparse.RequestParser()
-        parser.add_argument('user_name')
-        args = parser.parse_args()
-        user = UsersModel(db.get_connection()).get_by_name(args['user_name'])
-        return jsonify({'user': user})
+        if user_id == 0:
+            # поиск пользователя
+            parser = reqparse.RequestParser()
+            parser.add_argument('user_name')
+            args = parser.parse_args()
+            # получаем данные пользователя по имени
+            user = UsersModel(db.get_connection()).get_by_name(args['user_name'])
+            return jsonify({'user': user})
+        else:
+            # вывод статистики
+            posts = PostsModel(db.get_connection())
+            subs = SubsModel(db.get_connection())
+            return jsonify({'followers_count': subs.get_followers(user_id),
+                            'subscriptions_count': subs.get_subscriptions(user_id),
+                            'posts_count': posts.get_count(user_id)})
 
     def post(self, user_id):
         abort_if_not_authorized()
@@ -143,21 +173,24 @@ class Users(Resource):
         parser.add_argument('action')
         args = parser.parse_args()
         if args['action'] == 'subscribe':
+            # подписываемся
             SubsModel(db.get_connection()).subscribe(session['userid'], user_id)
             next_act = 'unsubscribe'
             text = 'Отписаться'
         elif args['action'] == 'unsubscribe':
+            # отписываемся
             SubsModel(db.get_connection()).unsubscribe(session['userid'], user_id)
             next_act = 'subscribe'
             text = 'Подписаться'
         return jsonify({'success': 'OK', 'next': next_act, 'text': text})
 
-api.add_resource(Users, '/api/users/<user_id>')
+api.add_resource(Users, '/api/users/<int:user_id>')
 
 
 '''
 ---===###  Flask URL handlers  ###===---
 '''
+
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
@@ -165,13 +198,17 @@ def login():
     login_error = ''
     if form.validate_on_submit():
         users = UsersModel(db.get_connection())
+        # проверяем существует ли пользователь с таким логином и паролем
         user = users.exists(form.username.data, form.password.data)
         if user[0]:
+            # выставляем id пользователя в сессию
             session['userid'] = users.get(user[1])[0]
             return redirect('/')
         else:
             login_error = 'Неправильный логин или пароль'
-    return render_template('project_login.html', title='Instagram', form=form, login_error=login_error)
+    return render_template('project_login.html', title='Instagram', form=form,
+                           login_error=login_error, user_info={'userid': 0})
+
 
 @app.route('/')
 def index():
@@ -183,9 +220,15 @@ def index():
         all_posts.append({'pub_date': datetime.fromtimestamp(i[5]).strftime('%d.%m.%Y %H:%M'),
                          'title': i[1], 'thumb': i[3], 'userid': i[4], 'pid': i[0]})
     users = UsersModel(db.get_connection())
+    subs = SubsModel(db.get_connection())
     user_data = users.get(session['userid'])
-    user_info = {'username': user_data[1], 'main_photo': user_data[4]}
-    return render_template('project_index.html', title='Instagram', posts=all_posts, user_info=user_info, sess_info=user_info)
+    user_info = {'username': user_data[1], 'main_photo': user_data[4],
+                 'followers_count': subs.get_followers(session['userid']),
+                 'subscriptions_count': subs.get_subscriptions(session['userid']),
+                 'posts_count': posts.get_count(session['userid']), 'userid': session['userid']}
+    return render_template('project_index.html', title='Instagram', posts=all_posts,
+                           user_info=user_info, sess_info=user_info)
+
 
 @app.route('/@<username>')
 def user_page(username):
@@ -200,10 +243,13 @@ def user_page(username):
         all_posts.append({'pub_date': datetime.fromtimestamp(i[5]).strftime('%d.%m.%Y %H:%M'),
                          'title': i[1], 'thumb': i[3], 'userid': i[4], 'pid': i[0]})
     user_data = users.get(userid)
-    user_info = {'username': user_data[1], 'main_photo': user_data[4], 'subscribed': subs.check_subscribed(session['userid'], userid), 'userid': user_data[0]}
+    user_info = {'username': user_data[1], 'main_photo': user_data[4],
+                 'subscribed': subs.check_subscribed(session['userid'], userid), 'userid': user_data[0]}
     user_data = users.get(session['userid'])
     sess_info = {'username': user_data[1], 'main_photo': user_data[4]}
-    return render_template('project_index.html', title='Instagram', posts=all_posts, user_info=user_info, sess_info=sess_info)
+    return render_template('project_index.html', title='Instagram',
+                           posts=all_posts, user_info=user_info, sess_info=sess_info)
+
 
 @app.route('/del_post/<pid>')
 def del_post(pid):
@@ -217,6 +263,7 @@ def del_post(pid):
         flash('Доступ запрещен', 'danger')
     return redirect('/')
 
+
 @app.route('/registration', methods=['GET', 'POST'])
 def register():
     form = RegisterForm()
@@ -228,7 +275,8 @@ def register():
             return redirect('/login')
         else:
             form.username.errors.append('Пользователь с таким именем уже существует')
-    return render_template('project_register.html', title='Instagram', form=form)
+    return render_template('project_register.html', title='Instagram', form=form, user_info={'userid': 0})
+
 
 @app.route('/upload_photo', methods=['GET', 'POST'])
 def upload_photo():
@@ -245,6 +293,7 @@ def upload_photo():
     user_data = users.get(session['userid'])
     user_info = {'username': user_data[1], 'main_photo': user_data[4]}
     return render_template('project_add_photos.html', form=form, user_info=user_info, sess_info=user_info)
+
 
 @app.route('/change_info', methods=['GET', 'POST'])
 def change_info():
@@ -268,6 +317,7 @@ def change_info():
     user_data = users.get(session['userid'])
     user_info = {'username': user_data[1], 'main_photo': user_data[4]}
     return render_template('project_change_info.html', form=form, user_info=user_info, sess_info=user_info)
+
 
 @app.route('/logout')
 def logout():
