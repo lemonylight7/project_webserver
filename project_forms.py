fc13704f584d84583937880ae46683d9f5396946
lemonylight7 <elizaveta.yermakova@gmail.com>
2019-03-14 00:24:50 +0300

    PEP8 fixes

--- project_forms.py
+++ project_forms.py
@@ -3,26 +3,31 @@ from flask_wtf.file import FileField, FileRequired
 from wtforms import StringField, SubmitField, TextAreaField, PasswordField, FileField
 from wtforms.validators import DataRequired, EqualTo
 
+
 class LoginForm(FlaskForm):
     username = StringField('Логин', validators=[DataRequired(message='Поле не должно быть пустым')])
     password = PasswordField('Пароль', validators=[DataRequired(message='Поле не должно быть пустым')])
     submit = SubmitField('Войти')
 
+
 class RegisterForm(FlaskForm):
     username = StringField('Логин', validators=[DataRequired(message='Поле не должно быть пустым')])
     password = PasswordField('Введите пароль', [DataRequired(message='Поле не должно быть пустым'),
-        EqualTo('confirm', message='Пароли должны совпадать')])
+                                                EqualTo('confirm', message='Пароли должны совпадать')])
     confirm = PasswordField('Повторите пароль', validators=[DataRequired(message='Поле не должно быть пустым')])
     submit = SubmitField('Зарегистрироваться')
+
 
 class UploadPhotoForm(FlaskForm):
     file = FileField('Загрузите фото', validators=[FileRequired(message='Поле не должно быть пустым')])
     descrypt = TextAreaField('Описание')
     submit = SubmitField('Загрузить')
 
+
 class ChangeInfoForm(FlaskForm):
     main_photo = FileField('Изменить главное фото')
     user_name = StringField('Изменить имя')
-    password_hash = PasswordField('Введите новый пароль', validators=[EqualTo('confirm', message='Пароли должны совпадать')])
+    password_hash = PasswordField('Введите новый пароль',
+                                  validators=[EqualTo('confirm', message='Пароли должны совпадать')])
     confirm = PasswordField('Повторите пароль')
     submit = SubmitField('Сохранить изменения')
